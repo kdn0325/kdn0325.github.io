f96271fa3ca14bbf2d319fd24b963fe50fe69162
Abel Kim <91298955+kdn0325@users.noreply.github.com>
2022-08-20 14:58:26 +0900

    #2

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "[Jekyll] 블로그 포스팅하는 방법",
+        "excerpt":"“title : 포스트의 제목을 큰 따옴표로 적어 준다. 이 title을 적어주지 않으면 .md 파일 이름으로 적어주었던 title 부분이 제목으로 업로드 된다. excerpt : 포스트 목록에서 보여지는 블로그 소개 글 categories : 이 포스트의 카테고리는 Blog로 정함. tags : 태그와 카테고리의 차이점은 카테고리는 sub url이 붙는 페이지가 있지만 태그는 없다는 것이다....","categories": ["Blog"],
+        "tags": ["Blog","jekyll","Github","Git"],
+        "url": "/blog/posting/",
+        "teaser": null
+      }]
